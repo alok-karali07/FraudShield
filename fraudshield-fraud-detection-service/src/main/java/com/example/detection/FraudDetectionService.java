8570b8fe173d9b4af2fc7617d0a5a2bd77d41e11
Alok <alokesparkbiz1626@gmail.com>
2026-05-01 15:20:59 +0530

    Removed unwanted config file.

--- fraudshield-fraud-detection-service/src/main/java/com/example/detection/FraudDetectionService.java
+++
@@ -1,9 +0,0 @@
-package com.example.detection;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafkaStreams;
-
-@Configuration
-@EnableKafkaStreams
-public class FraudDetectionService {
-}
