d04861156b2bc19ea0b582b3611712525d27e017
Alok <alokesparkbiz1626@gmail.com>
2026-05-01 15:53:59 +0530

    Added few annotations at entity level.

--- fraudshield-transaction-service/src/main/java/com/example/service/model/TransactionEntity.java
+++ fraudshield-transaction-service/src/main/java/com/example/service/model/TransactionEntity.java
@@ -11,12 +11,18 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Getter
 @Setter
 @Entity
